c07e6ff26397da5dec022894cb81f575fb815391
test <test>
2024-05-10 20:15:11 +0900

    컨트롤러 테스트

--- src/main/java/com/ProjectSC/main/MainController.java
+++ src/main/java/com/ProjectSC/main/MainController.java
@@ -1,14 +1,14 @@
-package com.ProjectSC.test;
+package com.ProjectSC.main;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-@RequestMapping("/test")
+@RequestMapping("/ProjectSC")
 @Controller
-public class TestController {
-	@GetMapping("/test")
-	public String test() {
-		return "test/test";
+public class MainController {
+	@GetMapping("/main")
+	public String main() {
+		return "main/main";
 	}
 }
---
+++ src/main/webapp/WEB-INF/jsp/main/main.jsp
@@ -0,0 +1,12 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>ProjectSC</title>
+</head>
+<body>
+	<h1>ProjectSC Main Page</h1>
+</body>
+</html>
